58998652d357c5b4ad38d13a0e9be42440e01327
Wanderson Guedes <guedesw3@gmail.com>
2022-05-04 11:08:08 -0300

    email comercial do ibti inserido

--- templates/forms/email.php
+++ templates/forms/email.php
@@ -1,11 +1,16 @@
 <?php
-    $para = "w1andersong2l3@gmail.com";
+    $para = "vendas@ibti.org.br";
     $nome = $_POST['name'];
     $email = $_POST['email'];
-    $telefone = $_POST['subject'];
+    $telefone = $_POST['telefone'];
     $assunto = $_POST['assunto'];
+    $empresa = $_POST['empresa'];
+    $cargo = $_POST['cargo'];
+    
     $mensagem = "<strong>Nome: </strong>".$nome;
     $mensagem .= "<br><strong>Email: </strong>".$email;
+    $mensagem .= "<br><strong>Empresa: </strong>".$empresa;
+    $mensagem .= "<br><strong>Cargo/Função: </strong>".$cargo;
     $mensagem .="<br> <strong> Mensagem: </strong>".$_POST['message'];
     $port = 465;
 
@@ -14,12 +19,13 @@
     $headers .= "X-Sender:  <smtp.gmail.com>\n";
     $headers .= "X-Mailer: PHP  v".phpversion()."\n";
     $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-    $headers .= "Return-Path:  <arley.souto@hotmail.com>\n";
+    $headers .= "Return-Path:  <vendas@ibti.org.br>\n";
     $headers .= "MIME-Version: 1.0\n";
-
-    if (!mail($para, $assunto, $mensagem, $headers, $port)){
+    
+    if (mail($para, $assunto, $mensagem, $headers, $port)){
         echo "Erro ao enviar";
     } else{
         echo  "<script>alert('Email enviado com Sucesso!');location.href=\"/index.html\";</script>";
     }
+
 ?>
